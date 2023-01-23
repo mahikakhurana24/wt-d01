2b4dc38a8649ff7772bb91f40ccab87757f8db5b
mahikakhurana24 <mahika.khurana.sod21@anu.edu.in>
2023-01-23 19:40:22 +0530

    fixed some code

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,17 +1,21 @@
-function Fibonacci() {
+function Fibonacci (){
+    const number = parseInt(ent.value);
     let n1 = 0, n2 = 1, nextTerm;
+
     var e=document.getElementById("ent").value;
-            var o=document.getElementById("out")
-            o.innerHTML += n1 +"<br>"; 
-            o.innerHTML += n2 +"<br>";
+    var o=document.getElementById("out")
+    o.innerHTML += n1 +"<br>"; 
+    o.innerHTML += n2 +"<br>";
 
-document.write('Fibonacci Series:' + "<br>");
+    // console.log('Fibonacci Series:');
+    // document.write('Fibonacci Series: <br>');
 
-for (let i = 1; i <= number; i++) 
-{
-    document.write(n1 + "<br>");
-    nextTerm = n1 + n2;
-    n1 = n2;
-    n2 = nextTerm;
-}
+    for (let i = 1; i <= number; i++) {
+        // console.log(n1);
+        o.innerHTML += n1 + "<br>"
+        // document.write(n1 + "<br>")
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
 }
